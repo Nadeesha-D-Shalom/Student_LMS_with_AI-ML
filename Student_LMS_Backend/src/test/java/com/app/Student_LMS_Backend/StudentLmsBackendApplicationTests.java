8157c2298f37bfbd43faffb8754839cf3f5cbb9d
Nadeesha Shalom <nadeeshashalom1@gmail.com>
2025-12-11 22:19:39 +0530

    Initial commit: add backend and frontend structure

---
+++ Student_LMS_Backend/src/test/java/com/app/Student_LMS_Backend/StudentLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Student_LMS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
